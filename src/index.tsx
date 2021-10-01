1e6178c3760a3a94d1fb7bcdf905c810dc1cbfc2
nik.pashmentov@ya.ru <nik.pashmentov@ya.ru>
2021-10-01 16:02:38 +0300

    Removed reportWebVitals

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import './index.scss';
 
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { rootReducer } from './Reducers';
@@ -15,8 +14,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
